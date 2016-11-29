4f2ffe394e7e8d8203fba5192f9d25f1605ce51b
Devon Williams <Willdewin@gmail.com>
2016-11-29 13:36:45 -0600

    Initial Commit

---
+++ Python/Basics.py
@@ -0,0 +1,5 @@
+my_string = 'this is a string stored in the my_string variable'
+my_num = 5
+my_tuple = (1,2,3,4,5)
+
+my_dictionary = {'name': 'Michael Choi', 'fave_long': 'Python', 'level': 'Sensei'}
